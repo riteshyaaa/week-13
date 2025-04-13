9eb05a6096429e2b158d8b3db73854de934e480c
riuteshyaaa <riteshyaaa@gmail.com>
2025-04-13 23:49:08 +0530

    Assignment-Sidebar functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 // const App = () => {
 //   return (
 //     <div>
@@ -22,13 +23,15 @@
 //   );
 // };
 
-import { DropDown } from "./components/Sidebar/dropDown";
-import { Sidebar } from "./components/Sidebar/Sidebar";
-import { SidebarToggle } from "./components/Sidebar/SideBarToggle";
+
 
 // export default App;
 
 // --------------------------------------------------------------------------------
+
+// import { DropDown } from "./components/Sidebar/dropDown";
+// import { Sidebar } from "./components/Sidebar/Sidebar";
+// import { SidebarToggle } from "./components/Sidebar/SideBarToggle";
 
 // const App =  () => {
 
@@ -72,11 +75,85 @@ import { SidebarToggle } from "./components/Sidebar/SideBarToggle";
 
 // ---------------------------------------------
 
-export function App() { 
+// export function App() {
+//   return (
+//     <div>
+
+//       <Sidebar />
+//     </div>
+//   );
+// }
+
+function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
   return (
-    <div>
-       
-      <Sidebar />
+    <div className="flex">
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <MainContent sidebarOpen={sidebarOpen} />
     </div>
   );
 }
+
+function SidebarToggle() {
+  return (
+    <div className="flex items-center justify-center w-0 md:w-10 h-10 bg-red-300 rounded-full">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="w-6 h-6 text-white"
+      >
+        <path d="M4 6h16M4 12h16m-7 6h7" />
+      </svg>
+    </div>
+  );
+}
+
+function Sidebar({ sidebarOpen, setSidebarOpen }) {
+  if (!sidebarOpen) {
+    return (
+      <div
+        className="fixed top-0 left-0 bg-slate-400  m-2  hover:bg-pink-200 rounded-full w-10 h-10 flex items-center justify-center   "
+        onClick={() => {
+          setSidebarOpen(!sidebarOpen);
+        }}
+      >
+        <SidebarToggle />
+      </div>
+    );
+  }
+  return (
+    <div className="w-0  md:w-96   h-screen bg-red-100">
+      <div>
+        <div
+          className="cursor-pointer  hover:bg-[#414a4c]  m-2 rounded-full w-10 h-10 flex items-center justify-center"
+          onClick={() => {
+            setSidebarOpen(!sidebarOpen);
+          }}
+        >
+          <SidebarToggle />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MainContent() {
+  return (
+    <div className="w-full">
+      <div className="h-72 bg-black"></div>
+      <div className="grid grid-cols-11 gap-8 p-8">
+        <div className="h-96 rounded-2xl  bg-red-200 col-span-2 -translate-y-24 shadow-lg"></div>
+        <div className="h-96 rounded-2xl  bg-green-200 col-span-6 shadow-lg"></div>
+        <div className="h-96 rounded-2xl  bg-yellow-200 col-span-3 shadow-lg"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
